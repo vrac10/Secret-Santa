a40a3133eec4d1e52ec843984552a3127bdae721
vrac10 <vrachit106@gmail.com>
2022-12-21 08:30:46 +0530

    fixed error

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import random
 names = {'Rahdwgdwgudwgdul' : 'rachit.prince11@gmail.com', 'Radwdiwhdiwhdiwm' : 'secretsanta.py12@gmail.com', 'Rachdit' : 'vrachit106@gmail.com'}
 l = []
 for i in names.keys():
-    name = random.choice(list(names.keys()))
-    if name in l or name == i:
+    while True:
         name = random.choice(list(names.keys()))
-    else:
-        l.append(name)
+        if name not in l or name != i:
+            break
     message = f"You will gift {name}"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
---
+++ randm.py
@@ -0,0 +1,6 @@
+import random 
+
+dict = {'a' : 'A', 'b' : 'B', 'c' : 'C'}
+l = []
+name = random.choice(list(dict.keys()))
+print(name)
