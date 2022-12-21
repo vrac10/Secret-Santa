ef9e0084566c86139f65cc9e4a87f7c67628c4b3
vrac10 <vrachit106@gmail.com>
2022-12-21 11:34:29 +0530

    Random names being generated without loop

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import smtplib,ssl
+import functools
 import random
 names = {'Rahdwgdwgudwgdul' : 'rachit.prince11@gmail.com', 'Radwdiwhdiwhdiwm' : 'secretsanta.py12@gmail.com', 'Rachdit' : 'vrachit106@gmail.com'}
-l = []
+name = random.sample(list(names.keys()),k=len(names))
+n = 0
 for i in names.keys():
-    while True:
-        name = random.choice(list(names.keys()))
-        if name not in l or name != i:
-            break
-    message = f"You will gift {name}"
+    message = f"You will gift {name[n]}"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login("secretsanta.py12@gmail.com", "qunmiyytnzlbwlnx")
         server.sendmail("secretsanta.py12@gmail.com", names[i], message)
+    n += 1
     
 
 
--- randm.py
+++
@@ -1,6 +0,0 @@
-import random 
-
-dict = {'a' : 'A', 'b' : 'B', 'c' : 'C'}
-l = []
-name = random.choice(list(dict.keys()))
-print(name)
