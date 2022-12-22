eefd60e3f3fc6f14b8ff693b313b759d27468af2
vrac10 <vrachit106@gmail.com>
2022-12-22 17:36:37 +0530

    Textfile readability feature  added

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import smtplib,ssl
 import random
-names = {'Rahdwgdwgudwgdul' : 'rachit.prince11@gmail.com', 'Radwdiwhdiwhdiwm' : 'secretsanta.py12@gmail.com', 'Rachdit' : 'vrachit106@gmail.com'}
+f = open("data.txt",'r')
+names = {i.split(',')[0] : i.split(',')[1].replace('\n', '') for i in f.readlines()}
 name = random.sample(list(names.keys()),k=len(names))
 n = 0
 for i in names.keys():
-    message = f"You will gift {name[n]}"
+    message = f"""
+    The secret santa event is being organised and 
+    You will be gifting {name[n]}"""
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login("secretsanta.py12@gmail.com", "qunmiyytnzlbwlnx")
         server.sendmail("secretsanta.py12@gmail.com", names[i], message)
     n += 1
-    
-
-
